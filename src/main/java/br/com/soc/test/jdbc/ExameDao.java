f3675f2ba05b959ea5f03b05ffb527aca3a3d555
Eric Yuzo <eyuzo@devsv.com.br>
2020-04-20 20:47:36 -0300

    adicionando novos metodos de consulta

--- src/main/java/br/com/soc/test/jdbc/ExameDao.java
+++ src/main/java/br/com/soc/test/jdbc/ExameDao.java
@@ -30,16 +30,16 @@ public class ExameDao {
 			String sql =
 					"SELECT EXM.ID_EXAME, EXM.DT_EXAME, EMP.ID_EMPRESA, EMP.NM_EMPRESA, EMP.CD_CNPJ " +
 					"      ,FUN.ID_FUNCIONARIO, FUN.NM_FUNCIONARIO, FUN.CD_CPF, FUN.CD_RG, FUN.CD_EMISSOR_RG " +
-					"      ,VEF.NM_SETOR, VEF.NM_CARGO, MED.ID_MEDICO, MED.NM_MEDICO, MED.CD_CRM " +
-					"      ,MED.SG_UF SG_UF_CRM, MED.NM_TITULO NM_TITULO_MEDICO " +
+					"      ,VEF.ID_EMPRESA_FUNCIONARIO, VEF.NM_SETOR, VEF.NM_CARGO, MED.ID_MEDICO, MED.NM_MEDICO " +
+					"      ,MED.CD_CRM, MED.SG_UF SG_UF_CRM, MED.NM_TITULO NM_TITULO_MEDICO " +
 					"      ,FIN.ID_FINALIDADE, FIN.NM_FINALIDADE, RES.ID_RESULTADO, RES.NM_RESULTADO " +
 					"FROM TB_EXAME EXM " +
 					"  JOIN TB_EMPRESA_FUNCIONARIO VEF " +
-					"    ON VEF.ID_EMPRESA = EXM.ID_EMPRESA AND VEF.ID_FUNCIONARIO = EXM.ID_FUNCIONARIO " +
+					"    ON VEF.ID_EMPRESA_FUNCIONARIO = EXM.ID_EMPRESA_FUNCIONARIO " +
 					"  JOIN TB_EMPRESA EMP " +
-					"    ON EMP.ID_EMPRESA = EXM.ID_EMPRESA " +
+					"    ON EMP.ID_EMPRESA = VEF.ID_EMPRESA " +
 					"  JOIN TB_FUNCIONARIO FUN " +
-					"    ON FUN.ID_FUNCIONARIO = EXM.ID_FUNCIONARIO " +
+					"    ON FUN.ID_FUNCIONARIO = VEF.ID_FUNCIONARIO " +
 					"  JOIN TB_MEDICO MED " +
 					"    ON MED.ID_MEDICO = EXM.ID_MEDICO " +
 					"  JOIN TB_FINALIDADE FIN " +
@@ -65,6 +65,7 @@ public class ExameDao {
 				funcionario.setOrgaoEmissorRg(rs.getString("CD_EMISSOR_RG"));
 				
 				VinculoEmpresaFuncionario participante = new VinculoEmpresaFuncionario();
+				participante.setId(rs.getInt("ID_EMPRESA_FUNCIONARIO"));
 				participante.setEmpresa(empresa);
 				participante.setFuncionario(funcionario);
 				participante.setSetor(rs.getString("NM_SETOR"));
@@ -98,6 +99,124 @@ public class ExameDao {
 			throw new RuntimeException(e);
 		}
 		return exames;
+	}
+	
+	public List<Finalidade> listaFinalidades() {
+		List<Finalidade> finalidades = new ArrayList<Finalidade>();
+		
+		try {
+			String sql = "SELECT * FROM TB_FINALIDADE;";
+			
+			PreparedStatement st = connection.prepareStatement(sql);
+			
+			ResultSet rs = st.executeQuery();
+			
+			while (rs.next()) {
+				Finalidade finalidade = new Finalidade();
+				finalidade.setId(rs.getInt("ID_FINALIDADE"));
+				finalidade.setNome(rs.getString("NM_FINALIDADE"));
+				
+				finalidades.add(finalidade);
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		return finalidades;
+	}
+	
+	public List<Resultado> listaResultados() {
+		List<Resultado> resultados = new ArrayList<Resultado>();
+		
+		try {
+			String sql = "SELECT * FROM TB_RESULTADO;";
+			
+			PreparedStatement st = connection.prepareStatement(sql);
+			
+			ResultSet rs = st.executeQuery();
+			
+			while (rs.next()) {
+				Resultado resultado = new Resultado();
+				resultado.setId(rs.getInt("ID_RESULTADO"));
+				resultado.setNome(rs.getString("NM_RESULTADO"));
+				
+				resultados.add(resultado);
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		return resultados;
+	}
+	
+	public List<Medico> listaMedicos() {
+		List<Medico> medicos = new ArrayList<Medico>();
+		
+		try {
+			String sql = "SELECT * FROM TB_MEDICO;";
+			
+			PreparedStatement st = connection.prepareStatement(sql);
+			
+			ResultSet rs = st.executeQuery();
+			
+			while (rs.next()) {
+				Medico medico = new Medico();
+				medico.setId(rs.getInt("ID_MEDICO"));
+				medico.setNome(rs.getString("NM_MEDICO"));
+				medico.setCrm(rs.getString("CD_CRM"));
+				medico.setUf(rs.getString("SG_UF"));
+				medico.setTitulo(rs.getString("NM_TITULO"));
+				
+				medicos.add(medico);
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		return medicos;
+	}
+	
+	public List<VinculoEmpresaFuncionario> listaFuncionariosGeral() {
+		List<VinculoEmpresaFuncionario> funcionarios = new ArrayList<VinculoEmpresaFuncionario>();
+		
+		try {
+			String sql =
+					"SELECT VEF.ID_EMPRESA_FUNCIONARIO, EMP.ID_EMPRESA, EMP.NM_EMPRESA, EMP.CD_CNPJ " +
+					"      ,FUN.ID_FUNCIONARIO, FUN.NM_FUNCIONARIO, FUN.CD_CPF, FUN.CD_RG, FUN.CD_EMISSOR_RG " +
+					"      ,VEF.NM_SETOR, VEF.NM_CARGO " +
+					"FROM TB_EMPRESA_FUNCIONARIO VEF " +
+					"  JOIN TB_EMPRESA EMP " +
+					"    ON EMP.ID_EMPRESA = VEF.ID_EMPRESA " +
+					"  JOIN TB_FUNCIONARIO FUN " +
+					"    ON FUN.ID_FUNCIONARIO = VEF.ID_FUNCIONARIO;";
+			
+			PreparedStatement st = connection.prepareStatement(sql);
+			
+			ResultSet rs = st.executeQuery();
+			
+			while (rs.next()) {
+				Empresa empresa = new Empresa();
+				empresa.setId(rs.getInt("ID_EMPRESA"));
+				empresa.setNome(rs.getString("NM_EMPRESA"));
+				empresa.setCnpj(rs.getString("CD_CNPJ"));
+	
+				Funcionario funcionario = new Funcionario();
+				funcionario.setId(rs.getInt("ID_FUNCIONARIO"));
+				funcionario.setNome(rs.getString("NM_FUNCIONARIO"));
+				funcionario.setCpf(rs.getString("CD_CPF"));
+				funcionario.setRg(rs.getString("CD_RG"));
+				funcionario.setOrgaoEmissorRg(rs.getString("CD_EMISSOR_RG"));
+				
+				VinculoEmpresaFuncionario participante = new VinculoEmpresaFuncionario();
+				participante.setId(rs.getInt("ID_EMPRESA_FUNCIONARIO"));
+				participante.setEmpresa(empresa);
+				participante.setFuncionario(funcionario);
+				participante.setSetor(rs.getString("NM_SETOR"));
+				participante.setCargo(rs.getString("NM_CARGO"));
+				
+				funcionarios.add(participante);
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		return funcionarios;
 	}
 	
 	public static void main(String[] args) {
